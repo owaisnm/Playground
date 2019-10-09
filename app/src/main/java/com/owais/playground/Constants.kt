2528c250b369ea091f6b75d996e25c5aa632c9dc
Owais Naeem <owaisn92@gmail.com>
2019-10-08 22:01:43 -0400

    made access key const

--- app/src/main/java/com/owais/playground/Constants.kt
+++ app/src/main/java/com/owais/playground/Constants.kt
@@ -7,6 +7,6 @@ class Constants {
         const val INITIAL_LOAD_SIZE: Int = 10
         const val PAGE_SIZE: Int = 10
         const val IMAGE_SIZE: Int = 250
-        val UNSPLASH_ACCESS_KEY: String = BuildConfig.UnsplashAccessKey
+        const val UNSPLASH_ACCESS_KEY: String = BuildConfig.UnsplashAccessKey
     }
 }
